24d54d9c4b0d54eef0aadde07c15600aa0c400b9
jonz94 <jody16888@gmail.com>
2022-08-29 20:22:47 +0800

    ✨ feat: upgrade nerd fonts to v2.2.1

--- download-source-glyphs.sh
+++ download-source-glyphs.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-NERD_FONTS_VERSION=${NERD_FONTS_VERSION:-v2.1.0}
+NERD_FONTS_VERSION=${NERD_FONTS_VERSION:-v2.2.1}
 upstream_src_glyphs_url="https://github.com/ryanoasis/nerd-fonts/blob/${NERD_FONTS_VERSION}/src/glyphs"
+
 glyphs=(
-  "FontAwesome.otf"
-  "NerdFontsSymbols 1000 EM Nerd Font Complete Blank.sfd"
-  "NerdFontsSymbols 2048 EM Nerd Font Complete Blank.sfd"
-  "Pomicons.otf"
-  "PowerlineExtraSymbols.otf"
-  "PowerlineSymbols.otf"
-  "Symbols Template 1000 em.ttf"
-  "Symbols Template 2048 em.ttf"
-  "Symbols-1000-em Nerd Font Complete.ttf"
-  "Symbols-2048-em Nerd Font Complete.ttf"
-  "Unicode_IEC_symbol_font.otf"
+  "codicons/codicon.ttf"
+  "font-awesome/FontAwesome.otf"
+  "powerline-symbols/PowerlineSymbols.otf"
+  "weather-icons/weathericons-regular-webfont.ttf"
   "devicons.ttf"
   "font-awesome-extension.ttf"
   "font-logos.ttf"
   "materialdesignicons-webfont.ttf"
+  "NerdFontsSymbols 1000 EM Nerd Font Complete Blank.sfd"
+  "NerdFontsSymbols 2048 EM Nerd Font Complete Blank.sfd"
   "octicons.ttf"
   "original-source.otf"
-  "weathericons-regular-webfont.ttf"
+  "Pomicons.otf"
+  "PowerlineExtraSymbols.otf"
+  "Symbols-1000-em Nerd Font Complete.ttf"
+  "Symbols-2048-em Nerd Font Complete.ttf"
+  "Unicode_IEC_symbol_font.otf"
 )
 
 mkdir -p src/glyphs
@@ -29,5 +29,5 @@ for glyph in "${glyphs[@]}"; do
   # replace all `whitespace` characters with `%20`
   percent_encoded_uri="${upstream_src_glyphs_url}/${glyph//\ /%20}?raw=true"
 
-  curl -fsSL ${percent_encoded_uri} --output "src/glyphs/${glyph}"
+  curl -fsSL ${percent_encoded_uri} --output "src/glyphs/${glyph}" --create-dirs
 done
